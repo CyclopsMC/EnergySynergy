bd8ea328e2e1a76218d79e0781196f67cf79364c
Ruben Taelman <rubensworks@gmail.com>
2017-01-04 19:32:09 +0100

    Bump mod version

--- src/main/java/org/cyclops/energysynergy/Reference.java
+++ src/main/java/org/cyclops/energysynergy/Reference.java
@@ -35,7 +35,7 @@ public class Reference {
     public static final String MOD_FORGE_VERSION_MIN = "12.18.1.2085";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
-    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.7.7";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.9.0";
     public static final String MOD_TESLA = "tesla";
     public static final String MOD_RF_API = "CoFHAPI";
     public static final String MOD_IC2 = "IC2";
